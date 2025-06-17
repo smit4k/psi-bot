8d70181103a3e2287fdeeba26b5e8ae73de47b01
smit4k <smit.patil.usa@gmail.com>
2025-06-17 16:42:48 -0400

    Update activity message

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ async fn main() {
                 println!("Bot is connected as {}", ready.user.name);
                 
                 // Set bot presence
-                let activity = ActivityData::watching("servers");
+                let activity = ActivityData::watching("over the server");
                 let status = OnlineStatus::Online;
                 ctx.set_presence(Some(activity), status);
                 
@@ -53,7 +53,7 @@ async fn main() {
 
 
 /// Checks the bot's latency
-#[poise::command(slash_command)]
+#[poise::command(prefix_command)]
 async fn ping(ctx: Context<'_>) -> Result<(), Error> {
     let shard_manager = ctx.framework().shard_manager();
 
